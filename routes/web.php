88083e46dfc79b3ac45645731f878901694fc7f5
Amir Abroun <abroon234@gmail.com>
2022-07-22 01:37:56 +0430

    feat: add blog page

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
