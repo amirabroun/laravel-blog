f28835f1300e53ad9e63c777aafe136ccf0ef455
Amir Abroun <abroon234@gmail.com>
2022-07-23 14:36:18 +0430

    chore: add public controller

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [App\Http\Controllers\PublicController::class, 'index']);
 
 Route::get('/welcome', function () {
     return view('welcome');
