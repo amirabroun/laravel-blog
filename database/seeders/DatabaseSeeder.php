8f4e5aeffaba5d71b3dc092cd437815779a847d4
Amir Abroun <abroon234@gmail.com>
2022-07-29 15:10:11 +0430

    feat: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\{Category, Post, User};
+use App\Models\{Category, Post, User, Label};
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
-        Category::factory(10)->create();
+        User::factory()->create([
+            'first_name' => 'Amir',
+            'last_name' => 'Abroun',
+            'email' => 'abroon234@gmail.com',
+            'password' => 12345678,
+            'is_admin' => 1,
+        ]); // admin user Amir Abroun
 
-        Post::factory(10)->sequence(
+        User::factory(rand(4, 20))->create();
+        Category::factory(rand(4, 20))->create();
+
+        // create some post with label
+        Post::factory(rand(4, 100))->hasAttached(
+            Label::factory(6)->create()
+        )->sequence(
+            fn () => [
+                'user_id' => User::all()->random(),
+                'category_id' => Category::all()->random(),
+            ]
+        )->create();
+
+        Post::factory(rand(3, 20))->hasAttached(
+            Label::factory(rand(3, 9))->create()
+        )->sequence(
             fn () => [
                 'user_id' => User::all()->random(),
                 'category_id' => Category::all()->random(),
