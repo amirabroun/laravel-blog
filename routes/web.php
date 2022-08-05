67a7a545df6d0448f080611f840c73a4ed6043c5
Amir Abroun <abroon234@gmail.com>
2022-08-05 16:38:24 +0430

    chore: update web routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Blog Routes Part
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
-Route::view('/welcome', 'welcome');
 
 Route::controller(App\Http\Controllers\AuthController::class)->group(function () {
     Route::prefix('users')->middleware('auth')->group(function () {
