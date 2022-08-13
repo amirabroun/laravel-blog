db8afad88d32728be402342aa17ccc86d144b9b8
Amir Abroun <abroon234@gmail.com>
2022-08-13 14:48:36 +0430

    chore: update web routes

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(App\Http\Controllers\PostController::class)->group(function () {
+    Route::prefix('posts')->group(function () {
+        Route::middleware(['auth', 'admin'])->group(function () {
+            Route::get('/create', 'create')->name('posts.create');
+            Route::get('/{id}/edit', 'edit')->name('posts.edit')->middleware(['auth', 'admin']);
+            Route::delete('/{id}', 'destroy')->name('posts.destroy');
+            Route::post('/', 'store')->name('posts.store');
+        });
+
+        Route::get('/{id}', 'show')->name('posts.show');
+    });
+
     Route::get('/', 'index')->name('posts.index');
     Route::get('/categories/{category_title}/posts',  'index')->name('categories.posts.index');
-
-    Route::prefix('posts')->group(function () {
-        Route::get('/create', 'create')->name('posts.create')->middleware(['auth', 'admin']);
-        Route::get('/{id}', 'show')->name('posts.show');
-        Route::delete('/{id}', 'destroy')->name('posts.destroy')->middleware(['auth', 'admin']);
-        Route::post('/', 'store')->name('posts.store')->middleware(['auth', 'admin']);
-    });
 });
 
 Route::prefix('categories')->controller(App\Http\Controllers\CategoryController::class)->group(function () {
-    Route::get('/create', 'create')->name('categories.create')->middleware(['auth', 'admin']);
+    Route::middleware(['auth', 'admin'])->group(function () {
+        Route::get('/create', 'create')->name('categories.create');
+        Route::post('/', 'store')->name('categories.store');
+    });
+
     Route::get('/{title}', 'show')->name('categories.show');
-    Route::post('/', 'store')->name('categories.store')->middleware(['auth', 'admin']);
 });
