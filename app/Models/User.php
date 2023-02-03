17861a4b14723ed1ca3be24f12ce15dde8d9cb0b
amirabroun <abroon234@gmail.com>
2023-02-03 15:44:47 +0330

    feat: add like relation and full_name append to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\{
+    Factories\HasFactory,
+    Casts\Attribute,
+    SoftDeletes
+};
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    protected $fillable = ['first_name', 'last_name', 'email', 'password', 'is_admin'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'is_admin'];
+
+    protected $appends = ['full_name'];
 
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    public function posts()
+    protected function fullName(): Attribute
     {
-        return $this->hasMany(Post::class);
+        $fullName = $this?->first_name . ' ' . $this?->last_name;
+
+        return Attribute::get(fn () => $fullName);
     }
 
     public function isAdmin()
@@ -34,5 +42,15 @@ class User extends Authenticatable
         $this->attributes['password'] = Hash::make($value);
 
         return $this;
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
