144e0406b2a695094ccd2334fd072e35ff49c9e4
amirabroun <abroon234@gmail.com>
2023-02-03 22:36:25 +0330

    feat: add uuid to categories table

--- database/migrations/2022_07_26_181405_create_blog_database.php
+++ database/migrations/2022_07_26_181405_create_blog_database.php
@@ -27,6 +27,7 @@ return new class extends Migration
 
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('title')->unique();
             $table->timestamps();
         });
