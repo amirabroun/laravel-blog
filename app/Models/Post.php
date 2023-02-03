79a6ad1371336b3376dd74c86ad1d6c14aa9bfe5
amirabroun <abroon234@gmail.com>
2023-02-03 15:41:14 +0330

    feat: add like service function to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,16 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{
+    Model,
+    Casts\Attribute,
+    Factories\HasFactory,
+};
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body', 'user_id', 'image_url', 'category_id', 'created_at', 'updated_at'];
+    protected $fillable = ['title', 'body', 'user_id', 'image_url', 'category_id'];
 
-    protected $with = ['user', 'labels'];
+    protected $appends = ['count_likes', 'can_auth_user_like_this_post'];
+
+    protected function countLikes(): Attribute
+    {
+        return Attribute::get(fn () => $this->likes()->count());
+    }
+
+    protected function canAuthUserLikeThisPost(): Attribute
+    {
+        /** @var User */
+        if (!$user = auth()->user()) {
+            return Attribute::get(fn () => false);
+        }
+
+        $get = $user->likes()
+            ->where('likeable_id', $this->id)
+            ->where('likeable_type', Post::class)
+            ->count() == 0;
+
+        return Attribute::get(fn () => $get);
+    }
 
     public function user()
     {
@@ -26,5 +49,34 @@ class Post extends Model
     public function labels()
     {
         return $this->belongsToMany(Label::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    /**
+     * @return Post
+     */
+    public function likePost(User $user)
+    {
+        $newLike = new Like;
+
+        $newLike->likeable()->associate($this)
+            ->user()->associate($user)
+            ->save();
+
+        return $this;
+    }
+
+    /**
+     * @return Post
+     */
+    public function disLikePost(User $user)
+    {
+        $user->likes()->whereMorphedTo('likeable', $this)->delete();
+
+        return $this;
     }
 }
