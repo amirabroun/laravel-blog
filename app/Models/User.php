eed181f52bf793e11249c1da4dd9a5485fa1e112
amirabroun <abroon234@gmail.com>
2023-02-04 15:13:08 +0330

    fix: hash password when save in database

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\{
     Factories\HasFactory,
     Casts\Attribute,
@@ -15,7 +16,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    protected $fillable = ['first_name', 'last_name', 'email', 'is_admin'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'password', 'is_admin'];
 
     protected $appends = ['full_name'];
 
@@ -29,6 +30,13 @@ class User extends Authenticatable
         $fullName = $this?->first_name . ' ' . $this?->last_name;
 
         return Attribute::get(fn () => $fullName);
+    }
+
+    protected function password(): Attribute
+    {
+        return Attribute::set(
+            fn ($password) => Hash::make($password)
+        );
     }
 
     public function posts()
