fcca71f4a7a5acd2f46fc8b64ce4d935af866f22
amirabroun <abroon234@gmail.com>
2023-02-06 12:07:34 +0330

    refactor: use the principle methods, names and middleware

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -7,8 +7,8 @@ Route::prefix('users')
     ->controller(AuthController::class)
     ->middleware('auth')
     ->group(function () {
-        Route::get('/', 'index')->middleware('admin')->name('users.index');
-        Route::get('/{id}', 'show')->name('users.profile.show');
+        Route::get('/', 'index')->name('users.index');
+        Route::get('/{id}', 'show')->withoutMiddleware('auth')->name('users.profile.show');
         Route::get('/{id}/edit', 'edit')->name('users.profile.edit');
         Route::put('/{id}', 'update')->name('users.profile.update');
     });
@@ -17,7 +17,7 @@ Route::prefix('register')
     ->controller(AuthController::class)
     ->middleware('guest')
     ->group(function () {
-        Route::view('/', 'auth.register')->name('register.index');
+        Route::get('/', fn () => view('auth.register'))->name('register.index');
         Route::post('/', 'register')->name('register');
     });
 
@@ -25,8 +25,8 @@ Route::prefix('login')
     ->controller(AuthController::class)
     ->middleware('guest')
     ->group(function () {
-        Route::view('/', 'auth.login')->name('login.index');
+        Route::get('/', fn () => view('auth.login'))->name('login.index');
         Route::post('/', 'login')->name('login');
     });
 
-Route::get('log-out', [AuthController::class, 'logout'])->middleware('auth')->name('log-out');
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
