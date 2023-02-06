900e64c2b2f056f2adbe7ef392d73afcdcff56a2
amirabroun <abroon234@gmail.com>
2023-02-06 15:11:09 +0330

    prov: in single user index blade posts view update

--- resources/views/singleUser/index.blade.php
+++ resources/views/singleUser/index.blade.php
@@ -14,78 +14,79 @@
     <div class="col mt-3 mb-5">
         <div class="row main-section">
             <div class="col-lg-1"></div>
-            <div class="col-sm-12 col-md-12 col-lg-7 mb-4 shadow-post">
-                @isset($posts)
-                @foreach ($posts as $post)
-                <div class="card mb-4">
-                    <div class="card-header text-muted" style="font-size: 13.5px;">
-                        @isset($post->created_at)
-                        <span class="text-muted mr-2" style="font-size: 13px;">{{ date('D F j, Y, G:i', strtotime($post->created_at)) }} </span>
-                        @endisset
-
-                        @if (auth()->user()?->ownerOrAdmin($post))
-                        <button class="toggler action-post-btn" style="font-size: 12px;" type="button" data-toggle="collapse" data-target="#SupportedContent-post-{{ $post->id }}" aria-controls="SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="toggler text-dark">act</span>
-                        </button>
-                        <div class="collapse" id="SupportedContent-post-{{ $post->id }}">
-                            <ul class="navbar-nav bg-light">
-                                <li class="nav-item">
-                                    <h1>
-                                        <hr>
-                                    </h1>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="{{ route('posts.edit', ['uuid' => $post->uuid, 'title' => $post->title]) }}" class="mr-3 text-info">
-                                        edit
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <h1></h1>
-                                </li>
-
-                                <li class="nav-item">
-                                    <form action="{{ route('posts.destroy', ['uuid' => $post->uuid]) }}" method="POST" id="delete-post-form-{{ $post->id }}">
-                                        @csrf @method('DELETE')
-                                    </form>
-                                    <a href="javascript:void(0)" class="mr-3 text-danger" onclick="$('#delete-post-form-{{ $post->id }}').submit()">
-                                        Delete
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+            @isset($posts)
+            <div class="col-sm-12 col-md-12 col-lg-7 mb-4  {{ !count($posts) ?: 'bg-light' }}">
+                @foreach ($posts as $key => $post)
+                <a class="text-dark" style="text-decoration: none;" href="{{ route('posts.show', ['uuid' => $post->uuid, 'title' => $post->title]) }}">
+                    <div class="card-body">
+                        @if(File::exists(base_path('/public/image/') . $post->image_url) && isset($post->image_url))
+                        <img class="card-img-top w-100" src="{{ URL::asset('/public/image/' . $post->image_url) }}" alt="#">
+                        <br>
+                        <br>
                         @endif
+
+                        <h2 class="card-title">
+                            {{ $post->title }}
+                        </h2>
+                        <p class="card-text">
+                            {{ $post->body }}
+                        </p>
                     </div>
-                    <a class="text-dark" style="text-decoration: none;" href="{{ route('posts.show', ['uuid' => $post->uuid, 'title' => $post->title]) }}">
-                        <div class="card-body">
-                            <h2 class="card-title">
-                                {{ $post->title }}
-                            </h2>
-                            @if(File::exists(base_path('/public/image/') . $post->image_url) && isset($post->image_url))
-                            <hr>
-                            <img class="card-img-top w-100" src="{{ URL::asset('/public/image/' . $post->image_url) }}" alt="#">
-                            <br>
-                            <br>
-                            @endif
-                            <p class="card-text">
-                                {{ $post->body }}
-                            </p>
-                        </div>
-                    </a>
+                </a>
+
+                <div class="card-footer bg-light text-muted" style="font-size: 13.5px; border: 0 solid rgba(0, 0, 0, 0)">
+                    @isset($post->created_at)
+                    <span class="text-muted mr-2" style="font-size: 13px;">{{ date('D F j, Y, G:i', strtotime($post->created_at)) }} </span>
+                    @endisset
+
+                    @if (auth()->user()?->ownerOrAdmin($post))
+                    <button class="toggler action-post-btn" style="font-size: 12px;" type="button" data-toggle="collapse" data-target="#SupportedContent-post-{{ $post->id }}" aria-controls="SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="toggler text-dark">act</span>
+                    </button>
+                    <div class="collapse" id="SupportedContent-post-{{ $post->id }}">
+                        <ul class="navbar-nav bg-light">
+                            <li class="nav-item">
+                                <h1>
+                                    <hr>
+                                </h1>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="{{ route('posts.edit', ['uuid' => $post->uuid, 'title' => $post->title]) }}" class="mr-3 text-info">
+                                    edit
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <h1></h1>
+                            </li>
+
+                            <li class="nav-item">
+                                <form action="{{ route('posts.destroy', ['uuid' => $post->uuid]) }}" method="POST" id="delete-post-form-{{ $post->id }}">
+                                    @csrf @method('DELETE')
+                                </form>
+                                <a href="javascript:void(0)" class="mr-3 text-danger" onclick="$('#delete-post-form-{{ $post->id }}').submit()">
+                                    Delete
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    @endif
                 </div>
+
+                @if(count($posts) != $key + 1)
+                <hr>
+                @endif
+
                 @endforeach
-                @endisset
             </div>
+            @endisset
             <div class="col-sm-12 col-md-12 col-lg-3 d-none d-lg-block d-xl-block">
-                <div class="card shadow-sm mb-4">
+                <div class="mb-4 bg-light">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item {{ !session('activeCategory') ? 'bg-light' : '' }}">
-                            <a style="font-size: 23px;" href="{{ route('posts.index') }}" class="{{ !session('activeCategory') ? 'text-danger' : 'text-dark' }}">
-                                #
-                                <span class="text-dark" href="{{ route('posts.index') }}" style="font-size: 17.8px;">
-                                    All
-                                </span>
+                            <a style="font-size: 17.8px;" href="{{ route('posts.index') }}" class="text-dark">
+                                All
                             </a>
                         </li>
 
@@ -94,9 +95,6 @@
                         @if(count($categories))
                         @foreach ($categories as $category)
                         <li class="list-group-item {{ session('activeCategory') == $category->id ? 'bg-light' : '' }}">
-                            <a style="font-size: 23px;" class="mr-2 {{ session('activeCategory') == $category->id ? 'text-danger' : 'text-dark' }}" href="{{ route('categories.posts', ['uuid' => $category->uuid, 'title' => $category->title]) }}">
-                                #
-                            </a>
                             <a class="text-dark" href="{{ route('categories.posts', ['uuid' => $category->uuid, 'title' => $category->title]) }}">
                                 {{ $category->title }}
                             </a>
