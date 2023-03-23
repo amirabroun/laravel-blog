c958940cd76dc61f14e8252182ade11557d89a3f
amirabroun <abroon234@gmail.com>
2023-03-23 23:36:27 +0330

    chore: delete unless middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'admin' => \App\Http\Middleware\Admin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
