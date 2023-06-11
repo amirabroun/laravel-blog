73bc74a160e9caf7072c94684e6a330b09ca63b1
amirabroun <abroon234@gmail.com>
2023-06-11 23:10:35 +0330

    pref: add user key to send a user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         return [
             'status' => self::HTTP_STATUS_CODE['success'],
             'message' => __('auth.successfully_auth'),
-            'data' => new UserResource($this->authUser)
+            'data' => ['user' => new UserResource($this->authUser)],
         ];
     }
 
@@ -44,7 +44,7 @@ class AuthController extends Controller
         return [
             'status' => self::HTTP_STATUS_CODE['success'],
             'message' => __('auth.login'),
-            'data' => new UserResource($user),
+            'data' => ['user' => new UserResource($user)],
         ];
     }
 
@@ -69,7 +69,7 @@ class AuthController extends Controller
         return [
             'status' => self::HTTP_STATUS_CODE['success'],
             'message' => __('auth.register'),
-            'data' => new UserResource($user),
+            'data' => ['user' => new UserResource($user)],
         ];
     }
 
