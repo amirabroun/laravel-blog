64c6793c2fd38e9538b2bc0256c60707e0ffa713
Amir Abroun <abroon234@gmail.com>
2023-12-21 13:23:57 +0330

    F

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -3,11 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use PhpTelegramBot\FluentKeyboard\InlineKeyboard\InlineKeyboardButton;
-use PhpTelegramBot\FluentKeyboard\InlineKeyboard\InlineKeyboardMarkup;
-use PhpTelegramBot\FluentKeyboard\ReplyKeyboard\KeyboardButton;
-use PhpTelegramBot\FluentKeyboard\ReplyKeyboard\KeyboardButtonPollType;
-use PhpTelegramBot\FluentKeyboard\ReplyKeyboard\ReplyKeyboardMarkup;
 use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -22,93 +17,31 @@ class TelegramController extends Controller
     public function __invoke(Request $request)
     {
         $userId = $request['message']['from']['id'];
+        $messageId = $request['message']['message_id'];
+        $newMessage = 'در حال دولوپ هستم :)';
 
-        $commandName = substr($this->getUserState($userId), 1);
-
-        Telegram::triggerCommand($commandName, Telegram::getWebhookUpdate());
-
-        Request::sendMessage([
-            'chat_id' => $userId,
-            'text' => 'Hello World',
-            'reply_markup' => ReplyKeyboardMarkup::make()
-                ->oneTimeKeyboard()
-                ->button(KeyboardButton::make('Cancel'))
-                ->button(KeyboardButton::make('OK')),
-        ]);
-
-        InlineKeyboardMarkup::make()
-            ->row([
-                InlineKeyboardButton::make('1')->callbackData('page-1'),
-                InlineKeyboardButton::make('2')->callbackData('page-2'),
-                InlineKeyboardButton::make('3')->callbackData('page-3')
-            ])
-            ->row([
-                InlineKeyboardButton::make('prev')->callbackData('page-prev'),
-                InlineKeyboardButton::make('next')->callbackData('page-next')
-            ]);
-
-        //
-        $keyboard = [
-            ['test1', 'test1', 'test1']
-        ];
-        $reply_markup = Keyboard::make([
-            'keyboard' => $keyboard,
+        $replyMarkup = Keyboard::make([
+            'inline_keyboard' => [
+                [
+                    [
+                        'text' => 'ok',
+                        'callback_data' => 'ok',
+                    ],
+                    [
+                        'text' => 'cancel',
+                        'callback_data' => 'cancel',
+                    ],
+                ]
+            ],
             'resize_keyboard' => true,
             'one_time_keyboard' => true
         ]);
 
-        $reply_markup->inlineButton();
-        $reply_markup->button([
-
-            [
-                'text' => 'hi',
-
-            ]
-        ]);
-
-
-        $response = Telegram::sendMessage([
+        Telegram::sendMessage([
             'chat_id' => $userId,
-            'text' => 'Hello World',
-            'reply_markup' => $reply_markup
-        ]);
-        $telegram = $request->all();
-        Telegram::bot()->sendMessage([
-            'chat_id' => $telegram['message']['from']['id'],
-            'text' => 'در حال دولوپ هستم :)',
-            'reply_to_message_id' => $telegram['message']['message_id'],
-            'reply_markup' => [
-                ['text' => 'btn_1'],
-                ['text' => 'btn_2'],
-            ],
-            [
-                ['text' => 'btn_3'],
-                ['text' => 'btn_4'],
-            ],
-            [
-                ['text' => 'btn_5'],
-            ]
-        ]);
-
-        return;
-
-        $keyBoard = new Keyboard();
-        $keyBoard->inline();
-
-        $keyBoard->button([
-
-            [
-                'text' => 'hi',
-
-            ]
-        ]);
-
-        $telegram = $request->all();
-        Telegram::bot()->sendMessage([
-            'chat_id' => $telegram['message']['from']['id'],
-            'text' => 'در حال دولوپ هستم :)',
-            'reply_to_message_id' => $telegram['message']['message_id'],
-            'reply_markup' => '',
+            'reply_to_message_id' => $messageId,
+            'text' => $newMessage,
+            'reply_markup' => $replyMarkup
         ]);
     }
 
@@ -126,5 +59,32 @@ class TelegramController extends Controller
         $state = in_array($state, self::STATES) ? $state : '/start';
 
         telegramCache("state.{$userId}", $state);
+    }
+
+    function storage(Request $request)
+    {
+        $userId = $request['message']['from']['id'];
+
+        $commandName = substr($this->getUserState($userId), 1);
+
+        Telegram::triggerCommand($commandName, Telegram::getWebhookUpdate());
+
+        Telegram::sendMessage([
+            'chat_id' => $userId,
+            'text' => 'Hello World',
+            'reply_markup' => Keyboard::make([
+                'inline_keyboard' => [
+                    [
+                        'text' => 'ok',
+                        'callback_data' => 'data_ok',
+                    ],
+                    [
+                        'text' => 'noO',
+                        'callback_data' => 'data_noO',
+                    ],
+                ],
+                'resize_keyboard' => true,
+            ])
+        ]);
     }
 }
