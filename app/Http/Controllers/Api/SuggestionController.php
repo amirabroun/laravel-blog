6807de292242f9be3de67eee53bdaed1db795abe
Amir Abroun <abroon234@gmail.com>
2023-12-28 12:53:23 +0330

    fix: update suggestion contoller query

--- app/Http/Controllers/Api/SuggestionController.php
+++ app/Http/Controllers/Api/SuggestionController.php
@@ -2,29 +2,40 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Post;
+use App\Models\{Post, User};
 
 class SuggestionController extends Controller
 {
     public function getSuggestionsUsers()
     {
-        $usersNotFollowed = $this->authUser->notFollowed()->inRandomOrder()
-            ->take(mt_rand(2, 5))->with('media')->get();
+        $userSuggestionsQuery = auth()->check()
+            ? auth()->user()->notFollowed()->take(mt_rand(2, 5))
+            : User::query()->take(mt_rand(8, 15));
+
+        $usersNotFollowed = $this->setAuthUserFollowStatus(
+            $userSuggestionsQuery->inRandomOrder()->with('media')->get()
+        );
 
         return [
             'status' => self::HTTP_STATUS_CODE['success'],
             'message' => __('app.users'),
-            'data' => ['users' => $this->setAuthUserFollowStatus($usersNotFollowed)],
+            'data' => ['users' => $usersNotFollowed],
         ];
     }
 
     public function getSuggestionsPosts()
     {
-        $posts = Post::query()->whereNot('user_id', $this->authUser->id)->with(['user', 'media'])->take(mt_rand(2, 4))->inRandomOrder()->whereHas(
+        $posts = Post::query()->when(
+            auth()->check(),
+            fn ($query) => $query->whereNot('user_id', auth()->id())
+        )->with(['user', 'media'])->take(mt_rand(2, 4))->inRandomOrder()->whereHas(
             'user',
-            fn ($query) => $query->whereDoesntHave('followers')->orWhereHas(
-                'followers',
-                fn ($query) => $query->whereNot('follower_id', $this->authUser->id)
+            fn ($query) => $query->whereDoesntHave('followers')->when(
+                auth()->check(),
+                fn ($query) => $query->orWhereHas(
+                    'followers',
+                    fn ($query) => $query->whereNot('follower_id', auth()->id())
+                )
             )
         )->get();
 
