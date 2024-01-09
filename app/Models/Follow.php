405b0a4c8f5aa71f4b152b4a089ec2efc6022fee
Amir Abroun <abroon234@gmail.com>
2024-01-09 14:01:57 +0330

    fix: add timestamps to fillable follow model

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -9,9 +9,7 @@ class Follow extends Model
 {
     protected $table = 'followables';
 
-    protected $fillable = ['followable_id', 'followable_type'];
-
-    protected $timestamps = ['accepted_at', 'created_at'];
+    protected $fillable = ['followable_id', 'followable_type', 'accepted_at', 'created_at'];
 
     public function followable(): MorphTo
     {
