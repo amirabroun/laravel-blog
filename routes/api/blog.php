b451fdd833eb685ea16ac7300af8d75cd61e87e8
Amir Abroun <abroon234@gmail.com>
2024-01-09 13:10:56 +0330

    chore: use optinial auth middleware for blog

--- routes/api/blog.php
+++ routes/api/blog.php
@@ -20,9 +20,7 @@ Route::prefix('posts')
     ->group(function () {
         Route::get('/', 'index')->name('posts.index');
 
-        Route::post('/', 'store')
-            ->middleware('auth:sanctum')
-            ->name('posts.store');
+        Route::post('/', 'store')->name('posts.store');
     });
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
@@ -31,34 +29,23 @@ Route::prefix('users')
     ->controller(UserController::class)
     ->whereUuid('uuid')
     ->group(function () {
-        Route::get('/', 'index')
-            ->middleware('auth:sanctum')
-            ->name('users.index');
+        Route::get('/', 'index')->name('users.index');
 
-        Route::get('{uuid}/posts', 'getUserPosts')
-            ->name('users.posts');
+        Route::get('{uuid}/posts', 'getUserPosts')->name('users.posts');
 
-        Route::get('{uuid}', 'show')
-            ->name('users.profile');
+        Route::get('{uuid}', 'show')->name('users.profile');
 
-        Route::put('{uuid}/update-profile', 'updateUserProfile')
-            ->middleware('auth:sanctum')
-            ->name('auth.profile.update');
+        Route::put('{uuid}/update-profile', 'updateUserProfile')->name('auth.profile.update');
 
-        Route::put('{uuid}/toggle-follow', 'toggleFollow')
-            ->middleware('auth:sanctum')
-            ->name('auth.users.toggleFollow');
+        Route::put('{uuid}/toggle-follow', 'toggleFollow')->name('auth.users.toggleFollow');
 
-        Route::put('{uuid}/update-resume', 'updateUserResume')
-            ->middleware('auth:sanctum')
-            ->name('auth.profile.update');
+        Route::put('{uuid}/update-resume', 'updateUserResume')->name('auth.profile.update');
     });
 
 Route::prefix('suggestions')
     ->controller(SuggestionController::class)
     ->group(function () {
-        Route::get('users', 'getSuggestionsUsers')
-            ->name('users.suggestions.users');
+        Route::get('users', 'getSuggestionsUsers')->name('users.suggestions.users');
 
         Route::get('posts', 'getSuggestionsPosts')->name('users.suggestions.posts');
     });
