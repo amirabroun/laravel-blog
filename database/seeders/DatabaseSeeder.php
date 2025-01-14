74e2ed41470421900cad9030ca783cdf7fa8815e
Amir Abroun <abroon234@gmail.com>
2025-01-14 22:15:54 +0330

    feat: add some static category and labels data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Database\Seeders\BlogSeeder\AdminSeeder;
 use Database\Seeders\BlogSeeder\GithubUsersSeeder;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +22,77 @@ class DatabaseSeeder extends Seeder
         $this->call(GithubUsersSeeder::class);
 
         Artisan::call('app:add-news');
+
+        $this->insertLabels();
+
+        $this->insertCategory();
+    }
+
+    private function insertLabels()
+    {
+        $labels = [
+            "Technology",
+            "Programming",
+            "Artificial Intelligence",
+            "Web Development",
+            "Cybersecurity",
+            "UI/UX Design",
+            "Digital Marketing",
+            "Data Science",
+            "Cloud Computing",
+            "Networking",
+            "Blockchain",
+            "Mobile Applications",
+            "Operating Systems",
+            "Robotics",
+            "Game Development",
+            "Algorithms",
+            "Machine Learning",
+            "Image Processing",
+            "Internet of Things",
+            "Data Analytics"
+        ];
+
+        foreach ($labels as $title) {
+            DB::table('labels')->insert([
+                'title' => $title,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+
+    private function insertCategory()
+    {
+        $categories = [
+            "Technology",
+            "Health",
+            "Education",
+            "Finance",
+            "Travel",
+            "Food",
+            "Lifestyle",
+            "Entertainment",
+            "Sports",
+            "Science",
+            "Environment",
+            "Art",
+            "History",
+            "Business",
+            "Gaming",
+            "Automobile",
+            "Fashion",
+            "Parenting",
+            "Real Estate",
+            "Politics"
+        ];
+
+        foreach ($categories as $title) {
+            DB::table('categories')->insert([
+                'title' => $title,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
