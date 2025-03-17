5ce02692a2cd6a9ea2c755f1ee230604a4292aaf
Amir Abroun <abroon234@gmail.com>
2025-03-17 12:04:33 +0330

    test: add ngrok http test on test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Api\News\Jcobhams;
+use App\Helper\DateOffsetParser;
 use App\Http\Controllers\Api\UserController;
 use App\Models\Post;
 use App\Models\User;
@@ -19,11 +20,39 @@ use App\Http\Resources\{PostResource, UserResource};
 use App\Models\Label;
 use App\Models\Task;
 use Database\Seeders\BlogSeeder\GithubUsersSeeder;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TestController
 {
     public function __invoke()
     {
-        return;
+        // $this->telegramWebhook();
+    }
+
+    private function telegramWebhook()
+    {
+        $ngrokUrl = 'https://978d-51-112-83-198.ngrok-free.app';
+        $botToken = config('telegram.bots.mybot.token');
+
+        $url = "$ngrokUrl/api/telegram/inputs/$botToken";
+
+        $apiUrl = "https://api.telegram.org/bot$botToken/setWebhook?url=" . $url;
+
+        $ch = curl_init($apiUrl);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($ch);
+
+
+        if ($response === false) {
+            dd('cURL error: ' . curl_error($ch));
+        } else {
+            dd('Response: ' . $response);
+        }
+
+        curl_close($ch);
     }
 }
